5215cb7a48d10426e59184fa5bb26234aebc7629
Noel Rostohar <noel.rostohar@gmail.com>
2020-08-20 15:36:18 +0200

    Enabled Redux devTools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,13 @@
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducers from 'src/store';
+import store from 'src/store';
 
 import { GlobalStyle } from '../styles';
 import { theme } from '../styles/theme';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
-  const store = createStore(reducers);
-
   return (
     <>
       <Head>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,21 @@
-import { combineReducers } from 'redux';
+import { combineReducers, createStore, compose } from 'redux';
 
 import { accessReducer } from './access/reducer';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const reducers = combineReducers({
   access: accessReducer,
 });
 
-export default reducers;
+const store = createStore(reducers, composeEnhancers());
+
+export default store;
