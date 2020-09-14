172039ce8571d5d61f7c20b425c2947e2b2c64f8
Noel Rostohar <noel.rostohar@gmail.com>
2020-09-14 20:53:54 +0200

    SSR Hydration Bug Fix v2

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import { ReactElement } from 'react';
 
 interface Props {
   styleTags: React.ReactNode;
@@ -31,17 +30,5 @@ export default class MyDocument extends Document<Props> {
     } finally {
       sheet.seal();
     }
-  }
-
-  render() {
-    return (
-      <html>
-        <Head>{this.props.styleTags}</Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
   }
 }
