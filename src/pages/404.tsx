25bb36e9f55ded6afc70d7e1471ffaf2ecc516c0
Noel Rostohar <noel.rostohar@gmail.com>
2020-09-15 10:09:25 +0200

    404 Page Design

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,29 @@
 import { NextPage } from 'next';
+import styled from 'styled-components';
+import Layout from 'src/components/Layout';
+
+const Container = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  margin: 35px auto;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Message = styled.h2`
+  color: white;
+`;
 
 const NotFound: NextPage = (): JSX.Element => {
-  return <></>;
+  return (
+    <Layout>
+      <Container>
+        <Message>Sorry, content not found.</Message>
+      </Container>
+    </Layout>
+  );
 };
 
 export default NotFound;
